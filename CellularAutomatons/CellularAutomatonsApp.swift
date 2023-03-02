bd2b1360b2668d76e0a7d08db8bc78dd4f92d546
Steven Wijaya <steven.wijaya2001@gmail.com>
2023-03-02 09:28:46 +0700

    Initial Commit

---
+++ CellularAutomatons/CellularAutomatonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellularAutomatonsApp.swift
+//  CellularAutomatons
+//
+//  Created by Steven Wijaya on 02.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CellularAutomatonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
